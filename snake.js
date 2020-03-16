fa7d7394bbdf85a3956470ea434a8776ccd9c237
Denny Pearce <obsidian.y.dragon@gmail.com>
2020-03-16 23:48:27 +0200

    realize draw function

--- snake.js
+++ snake.js
@@ -28,3 +28,19 @@ let food = {
 let score = 0;
 
 // draw everything to the canvas
+function draw() {
+  ctx.drawImage(ground, 0, 0);
+
+  for (let i = 0; i < snake.length; i++) {
+    ctx.fillStyle = i == 0 ? "green" : "white";
+    ctx.fillRect(snake[i].x, snake[i].y, box, box);
+
+    ctx.strokeStyle = "red";
+    ctx.strokeRect(snake[i].x, snake[i].y, box, box);
+  }
+
+  ctx.drawImage(foodImg, food.x, food.y);
+}
+
+// call draw function every 100ms
+let game = setInterval(draw, 100);
