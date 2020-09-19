aa1e44df2078809fe912f6dfa49f4dd11c4f667d
Denny Pearce <obsidian.y.dragon@gmail.com>
2020-09-19 12:43:28 +0300

    include images to js

--- js/snake.js
+++ js/snake.js
@@ -3,3 +3,10 @@ const ctx = cvs.getContext("2d");
 
 // create the unit
 const box = 32;
+
+// load images
+const ground = new Image();
+ground.src = "../img/ground.png";
+
+const foodImg = new Image();
+foodImg.src = "../img/food.png";
