4c49732f6e33b6a3c23919a40834007e374e9572
Denny Pearce <obsidian.y.dragon@gmail.com>
2020-09-19 12:44:03 +0300

    add pathes for audio

--- js/snake.js
+++ js/snake.js
@@ -18,3 +18,10 @@ const up = new Audio();
 const right = new Audio();
 const left = new Audio();
 const down = new Audio();
+
+dead.src = "../audio/dead.mp3";
+eat.src = "../audio/eat.mp3";
+up.src = "../audio/up.mp3";
+right.src = "../audio/right.mp3";
+left.src = "../audio/left.mp3";
+down.src = "../audio/down.mp3";
