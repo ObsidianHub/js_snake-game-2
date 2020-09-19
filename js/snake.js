62c843e13463b5be40761d1b36ea58a4fb0b8f2e
Denny Pearce <obsidian.y.dragon@gmail.com>
2020-09-19 13:46:51 +0300

    if snake eats food -> add score, play sound and generate new food

--- js/snake.js
+++ js/snake.js
@@ -97,6 +97,20 @@ function draw() {
   if (d == "UP") snakeY -= box;
   if (d == "RIGHT") snakeX += box;
   if (d == "DOWN") snakeY += box;
+
+  // if the snake eats the food
+  if (snakeX == food.x && snakeY == food.y) {
+    score++;
+    eat.play();
+    food = {
+      x: Math.floor(Math.random() * 17 + 1) * box,
+      y: Math.floor(Math.random() * 15 + 3) * box,
+    };
+    // we don't remove the tail
+  } else {
+    // remove the tail
+    snake.pop();
+  }
 }
 
 // call draw function every 100ms
