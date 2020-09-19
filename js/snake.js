06c3f3db83db5e484e536d2fb6852ae5c728a64f
Denny Pearce <obsidian.y.dragon@gmail.com>
2020-09-19 13:49:57 +0300

    fix pathes, delete extra file

--- js/snake.js
+++ js/snake.js
@@ -6,10 +6,10 @@ const box = 32;
 
 // load images
 const ground = new Image();
-ground.src = "../img/ground.png";
+ground.src = "img/ground.png";
 
 const foodImg = new Image();
-foodImg.src = "../img/food.png";
+foodImg.src = "img/food.png";
 
 // load audio files
 const dead = new Audio();
@@ -19,12 +19,12 @@ const right = new Audio();
 const left = new Audio();
 const down = new Audio();
 
-dead.src = "../audio/dead.mp3";
-eat.src = "../audio/eat.mp3";
-up.src = "../audio/up.mp3";
-right.src = "../audio/right.mp3";
-left.src = "../audio/left.mp3";
-down.src = "../audio/down.mp3";
+dead.src = "audio/dead.mp3";
+eat.src = "audio/eat.mp3";
+up.src = "audio/up.mp3";
+right.src = "audio/right.mp3";
+left.src = "audio/left.mp3";
+down.src = "audio/down.mp3";
 
 // create the snake
 let snake = [];
